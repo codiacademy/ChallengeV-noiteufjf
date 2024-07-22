c688943910bfe274749e04fec579c4d60ddfca0a
Patrício Samuel <patricioanjosjf@gmail.com>
2024-07-22 13:08:00 -0300

    Ajuste de conflitos de estilização

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,32 +6,32 @@ export default function Home() {
     return (
         <>
             {/* Hero */}
-            <section id="hero-section">
+            <section id="hero-section" className='home-section'>
                 <h1 id='title'>Home</h1>
             </section>
 
             {/* Nossos serviços */}
-            <section id="services-section">
+            <section id="services-section" className='home-section'>
                 <h1 id='title'>Nossos serviços</h1>
             </section>
 
             {/* Sobre nós */}
-            <section id="about-section">
+            <section id="about-section" className='home-section'>
                 <h1 id='title'>Sobre nós</h1>
             </section>
 
             {/* Reconhecimento */}
-            <section id="recognition-section">
+            <section id="recognition-section" className='home-section'>
                 <h1 id='title'>Reconhecimento</h1>
             </section>
 
             {/* O que nossos clientes dizem */}
-            <section id="testimony-section">
+            <section id="testimony-section" className='home-section'>
                 <h1 id='title'>Clientes dizem</h1>
             </section>
 
             {/* Entre em contato */}
-            <section id="contact-section">
+            <section id="contact-section" className='home-section'>
                 <h1 id='title'>Entre em Contato</h1>
                 <div className="contentContact">
                     <div className="formCenter">
