232103e839995773f7b176174e0b68c00acabdef
Patrício Samuel <patricioanjosjf@gmail.com>
2024-08-03 12:55:27 -0300

    Adicionei as pessoas do time que faltavam

--- frontend/src/pages/Team/Team.jsx
+++ frontend/src/pages/Team/Team.jsx
@@ -24,12 +24,32 @@ export default function Team() {
                     description={"Descrição da experiência e habilidades do membro da equipe."}
                 />
                 <TeamCard
+                    name={"Leonardo Costa"}
+                    role={"Desenvolvedor"}
+                    description={"Descrição da experiência e habilidades do membro da equipe."}
+                />
+                <TeamCard
+                    name={"Lucas"}
+                    role={"Desenvolvedor"}
+                    description={"Descrição da experiência e habilidades do membro da equipe."}
+                />
+                <TeamCard
+                    name={"Natan"}
+                    role={"Desenvolvedor"}
+                    description={"Descrição da experiência e habilidades do membro da equipe."}
+                />
+                <TeamCard
                     name={"Nícolas"}
                     role={"Desenvolvedor"}
                     description={"Descrição da experiência e habilidades do membro da equipe."}
                 />
                 <TeamCard
                     name={"Patrício Samuel"}
+                    role={"Desenvolvedor"}
+                    description={"Descrição da experiência e habilidades do membro da equipe."}
+                />
+                <TeamCard
+                    name={"Vinícius"}
                     role={"Desenvolvedor"}
                     description={"Descrição da experiência e habilidades do membro da equipe."}
                 />
