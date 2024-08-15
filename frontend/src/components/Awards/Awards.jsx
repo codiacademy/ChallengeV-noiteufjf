2c5799ca9d15b66ee7a076329d76174ace980e5c
esthermoraes777 <esther.gtalk@gmail.com>
2024-08-15 20:41:56 -0300

    css reconhecimento responsivo

--- frontend/src/components/Awards/Awards.jsx
+++ frontend/src/components/Awards/Awards.jsx
@@ -6,10 +6,10 @@ import './awards.css'
 export default function Awards() {
 
     return (
-        <div>
-        <div class="certificates">
+        <div className="main">
+        <div className="certificates">
             <img src={certificado} alt="Certificado 1"></img>
-            <img src={reconhecimento} alt="Equipe segurando certificado"></img>
+            <img id="img-center"src={reconhecimento} alt="Equipe segurando certificado"></img>
             <img src={certificado2} alt="Certificado 2"></img>
         </div>
         <p className="recognation-text">Fomos eleitos em 2023 como a “Melhor Escola de Programação de Juiz de Fora”, com votação popular pelo instituto Idealize. Também recebemos o prêmio de “Melhor Escola de Informática/Tecnologia” da Zona da Mata Mineira pelo instituto Previlige.</p>
