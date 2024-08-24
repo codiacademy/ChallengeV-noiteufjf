4ea49eaa5785bebd9f48fb217f7f5d62cd45a6c3
Patrício Samuel <patricioanjosjf@gmail.com>
2024-08-24 13:17:09 -0300

    style: ajuste de estilo no modal

--- frontend/src/components/Admin/Modal/Modal.jsx
+++ frontend/src/components/Admin/Modal/Modal.jsx
@@ -6,15 +6,17 @@ export default function Modal({ isModalOpen, setIsModalOpen, Component }) {
     if (!Component) {
         return null;
     }
-    
+
     return (
-        <dialog open={isModalOpen} className="mx-auto fixed inset-y-40 p-4 rounded-md w-[50%] min-w-72 shadow-lg">
-            <button onClick={() => setIsModalOpen(false)} className="absolute right-2 text-red-700">
-                <X />
-            </button>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Component />
-            </Suspense>
-        </dialog>
+        <div className={`fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center overflow-y-auto z-20 ${isModalOpen ? 'block' : 'hidden'}`}>
+            <div className="bg-white p-4 rounded-md w-[50%] min-w-72 shadow-lg relative m-auto lg:m-0">
+                <button onClick={() => setIsModalOpen(false)} className="absolute right-2 text-red-700">
+                    <X />
+                </button>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Component />
+                </Suspense>
+            </div>
+        </div>
     )
 }
