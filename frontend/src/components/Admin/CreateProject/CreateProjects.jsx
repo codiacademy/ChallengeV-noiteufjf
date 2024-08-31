0d9b1208abfa9b8f32c84ecec8c800d5b17e37bc
lucasvrcintra <lucasrcintra@hotmail.com>
2024-08-30 22:45:57 -0300

    adicionando toasts e ajustes modal

--- frontend/src/components/Admin/CreateProject/CreateProjects.jsx
+++ frontend/src/components/Admin/CreateProject/CreateProjects.jsx
@@ -1,148 +1,169 @@
-import { useState } from 'react'
-import { api } from '../../../lib/api'
+import { ManageUsersContext } from "../../../context/AppProvider";
+import { useState } from "react";
+import { api } from "../../../lib/api";
+import { toast } from "react-toastify";
+import { useContext } from "react";
 
 export default function CreateProject({ closeModal }) {
-    const [inputData, setInputData] = useState({
-        name: '',
-        cnpj: '',
-        status: '',
-        progress: '',
-        client: '',
-        userId: ''
-    })
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        const trimmedData = {
-            name: inputData.name.trim(),
-            cnpj: inputData.cnpj.trim(),
-            status: inputData.status.trim(),
-            progress: inputData.progress.trim(),
-            client: inputData.client.trim(),
-            userId: inputData.userId.trim()
-            
-        }
-        api.post('/projects', trimmedData)
-        .then(response => {
-            console.log("Projeto criado com sucesso!", response.data);
-            alert("Projeto criado com sucesso!");
-            clearInputs();
-            closeModal();
-        }).catch(error => {
-            const errorMessage = error.response?.data?.message || error.message || "Erro desconhecido";
-            console.error("Erro ao criar projeto:", errorMessage);
-            alert("Erro ao criar o projeto: " + errorMessage);
-        }); 
+  const notify = (message, type) => {
+    if (type === "success") {
+      toast.success(message);
+    } else if (type === "error") {
+      toast.error(message);
     }
+  };
 
+  const { fetchProjects } = useContext(ManageUsersContext);
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target
+  const [inputData, setInputData] = useState({
+    name: "",
+    cnpj: "",
+    status: "",
+    progress: "",
+    client: "",
+    userId: "",
+  });
 
-        setInputData(prevState => ({
-            ...prevState,
-            [name]: value
-        }))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const clearInputs = () => {
-        setInputData({
-            name: '',
-            cnpj: '',
-            status: '',
-            progress: '',
-            client: '',
-            userId: ''
-        })
-    }
+    const trimmedData = {
+      name: inputData.name.trim(),
+      cnpj: inputData.cnpj.trim(),
+      status: inputData.status.trim(),
+      progress: inputData.progress.trim(),
+      client: inputData.client.trim(),
+      userId: inputData.userId.trim(),
+    };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2 className='text-2xl text-center'>Criar Projeto</h2>
-            <section className='flex flex-wrap justify-center gap-x-4'>
-                <div className='flex-auto'>
-                    <div>
-                        <label htmlFor='name' className='block'>Nome do Projeto</label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            placeholder='Nome do projeto'
-                            required
-                            onChange={handleInputChange}
-                            value={inputData.name}
-                            className="w-full"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='cnpj' className='block'>CNPJ</label>
-                        <input
-                            type="text"
-                            name="cnpj"
-                            id="cnpj"
-                            placeholder='CNPJ'
-                            required
-                            onChange={handleInputChange}
-                            value={inputData.cnpj}
-                            className="w-full"
-                        />
-                    </div>
-                    
-                </div>
-                <div className='flex-auto'>
+    api
+      .post("/projects", trimmedData)
+      .then((response) => {
+        notify(response.data, "success");
+        clearInputs();
+        closeModal();
+        fetchProjects();
+      })
+      .catch((error) => {
+        const errorMessage =
+          error.response?.data?.message || error.message || "Erro desconhecido";
 
-                <div>
-                        <label htmlFor='client' className='block'>Cliente</label>
-                        <input
-                            type="text"
-                            name="client"
-                            id="client"
-                            placeholder='Cliente'
-                            required
-                            onChange={handleInputChange}
-                            value={inputData.client}
-                            className="w-full"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='progress' className='block'>Progresso</label>
-                        <input
-                            type="text"
-                            name="progress"
-                            id="progress"
-                            placeholder='Progresso'
-                            required
-                            onChange={handleInputChange}
-                            value={inputData.progress}
-                            className="w-full"
-                        />
-                    </div>
+        notify(errorMessage, "error");
+      });
+  };
 
-                    <div>
-                        <label htmlFor='userId' className='block'>ID do cliente</label>
-                        <input
-                            type="text"
-                            name="userId"
-                            id="userId"
-                            placeholder='Identificador do usuário'
-                            required
-                            onChange={handleInputChange}
-                            value={inputData.userId}
-                            className="w-full"
-                        />
-                    </div>
-                    
-                </div>
-            </section>
-            <button
-            type='submit'
-                className="w-full rounded-md bg-purple-600 px-4 py-2 font-medium text-lg text-gray-50 transition-colors hover:bg-purple-600/60"
-                aria-label='Cadastrar Projeto'
-                
-            >
-                Criar Projeto
-            </button>
-        </form>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const clearInputs = () => {
+    setInputData({
+      name: "",
+      cnpj: "",
+      status: "",
+      progress: "",
+      client: "",
+      userId: "",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="text-2xl text-center">Criar Projeto</h2>
+      <section className="flex flex-wrap justify-center gap-x-4">
+        <div className="flex-auto">
+          <div>
+            <label htmlFor="name" className="block">
+              Nome do Projeto
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nome do projeto"
+              required
+              onChange={handleInputChange}
+              value={inputData.name}
+              className="w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="cnpj" className="block">
+              CNPJ
+            </label>
+            <input
+              type="text"
+              name="cnpj"
+              id="cnpj"
+              placeholder="CNPJ"
+              required
+              onChange={handleInputChange}
+              value={inputData.cnpj}
+              className="w-full"
+            />
+          </div>
+        </div>
+        <div className="flex-auto">
+          <div>
+            <label htmlFor="client" className="block">
+              Cliente
+            </label>
+            <input
+              type="text"
+              name="client"
+              id="client"
+              placeholder="Cliente"
+              required
+              onChange={handleInputChange}
+              value={inputData.client}
+              className="w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="progress" className="block">
+              Progresso
+            </label>
+            <input
+              type="text"
+              name="progress"
+              id="progress"
+              placeholder="Progresso"
+              required
+              onChange={handleInputChange}
+              value={inputData.progress}
+              className="w-full"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="userId" className="block">
+              ID do cliente
+            </label>
+            <input
+              type="text"
+              name="userId"
+              id="userId"
+              placeholder="Identificador do usuário"
+              required
+              onChange={handleInputChange}
+              value={inputData.userId}
+              className="w-full"
+            />
+          </div>
+        </div>
+      </section>
+      <button
+        type="submit"
+        className="w-full rounded-md bg-purple-600 px-4 py-2 font-medium text-lg text-gray-50 transition-colors hover:bg-purple-600/60"
+        aria-label="Cadastrar Projeto"
+      >
+        Criar Projeto
+      </button>
+    </form>
+  );
 }
--- frontend/src/components/Admin/EditProjects/EditProjects.jsx
+++ frontend/src/components/Admin/EditProjects/EditProjects.jsx
@@ -2,165 +2,177 @@
 import { ManageUsersContext } from "../../../context/AppProvider";
 import { api } from "../../../lib/api";
 import { useEffect, useState, useContext } from "react";
+import { toast } from "react-toastify";
 
 export default function EditProjects({ project, closeModal }) {
-    const { fetchProjects } = useContext(ManageUsersContext)
-    const [inputData, setInputData] = useState({
-        name: '',
-        status: '',
-        cnpj: '',
-        progress: '',
-    });
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-
-        setInputData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
-    const handleStatusChange = (e) => {
-        const { value } = e.target;
-
-        setInputData(prevState => ({
-            ...prevState,
-            status: value  // Directly set the status to the selected value
-        }));
-    };
-
-
-    useEffect(() => {
-        if (project) {
-            setInputData({
-                name: project.name || '',
-                status: project.status ? project.status.split(",") : [] || '',
-                progress: project.progress || '',
-                cnpj: project.cnpj || '',
-
-            });
-        }
-    }, [project]);
-
-    const clearInputs = () => {
-        setInputData({
-            name: '',
-            status: '',
-            cnpj: '',
-            progress: ''
-        })
+  const notify = (message, type) => {
+    if (type === "success") {
+      toast.success(message);
+    } else if (type === "error") {
+      toast.error(message);
     }
+  };
+  const { fetchProjects } = useContext(ManageUsersContext);
+  const [inputData, setInputData] = useState({
+    name: "",
+    status: "",
+    cnpj: "",
+    progress: "",
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-        const trimmedData = {
-            name: inputData.name.trim(),
-            cnpj: inputData.cnpj.trim(),
-            status: inputData.status.trim(), // Corrigido para transformar o array em string
-            progress: inputData.progress.trim(),
-        };
+    setInputData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-        api.put(`/projects/${project.id}`, trimmedData)
-            .then(response => {
-                console.log("Projeto atualizado com sucesso!", response.data);
-                clearInputs();
-                closeModal();
-                fetchProjects();
-            }).catch(error => {
-                console.error("Não foi possível atualizar o projeto. Erro: ", error.response?.data?.message || error.message || "Erro desconhecido");
-            });
+  const handleStatusChange = (e) => {
+    const { value } = e.target;
+
+    setInputData((prevState) => ({
+      ...prevState,
+      status: value, // Directly set the status to the selected value
+    }));
+  };
+
+  useEffect(() => {
+    if (project) {
+      setInputData({
+        name: project.name || "",
+        status: project.status ? project.status.split(",") : [] || "",
+        progress: project.progress || "",
+        cnpj: project.cnpj || "",
+      });
+    }
+  }, [project]);
+
+  const clearInputs = () => {
+    setInputData({
+      name: "",
+      status: "",
+      cnpj: "",
+      progress: "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const trimmedData = {
+      name: inputData.name.trim(),
+      cnpj: inputData.cnpj.trim(),
+      status: inputData.status.trim(), // Corrigido para transformar o array em string
+      progress: inputData.progress.trim(),
     };
 
+    api
+      .put(`/projects/${project.id}`, trimmedData)
+      .then((response) => {
+        console.log("Projeto atualizado com sucesso!", response.data);
+        notify(response.data, "success");
+        clearInputs();
+        closeModal();
+        fetchProjects();
+      })
+      .catch((error) => {
+        console.error(
+          "Não foi possível atualizar o projeto. Erro: ",
+          error.response?.data?.message || error.message || "Erro desconhecido"
+        );
+        notify(
+          error.response?.data?.message || error.message || "Erro desconhecido",
+          "error"
+        );
+      });
+  };
 
+  return (
+    <div className="mt-4">
+      <div className="grid gap-1 mb-4">
+        <label htmlFor="name">Projeto</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={inputData.name}
+          onChange={handleInputChange}
+          className="border border-gray-300 rounded-md p-1"
+        />
+      </div>
 
-    return (
-        <div className="mt-4">
-            <div className="grid gap-1 mb-4">
-                <label htmlFor="name">Projeto</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={inputData.name}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 rounded-md p-1"
-                />
-            </div>
-
-            <div className="grid gap-1 mb-4">
-                <label>Status</label>
-                <div className="flex items-center gap-4">
-                    <label className="flex items-center ">
-                        <input
-                            type="radio"
-                            name="status"
-                            value="em-desenvolvimento"
-                            checked={inputData.status === 'em-desenvolvimento'}
-                            onChange={handleStatusChange}
-                            className="form-radio text-purple-600 cursor-pointer"
-                        />
-                        <span className="ml-2">Em desenvolvimento</span>
-                    </label>
-                    <label className="flex items-center">
-                        <input
-                            type="radio"
-                            name="status"
-                            value="em-testes"
-                            checked={inputData.status === 'em-testes'}
-                            onChange={handleStatusChange}
-                            className="form-radio text-purple-600 cursor-pointer"
-                        />
-                        <span className="ml-2">Em testes</span>
-                    </label>
-                    <label className="flex items-center">
-                        <input
-                            type="radio"
-                            name="status"
-                            value="finalizado"
-                            checked={inputData.status === 'finalizado'}
-                            onChange={handleStatusChange}
-                            className="form-radio text-purple-600 cursor-pointer"
-                        />
-                        <span className="ml-2">Finalizado</span>
-                    </label>
-                </div>
-            </div>
-
-            <div className="grid gap-1 mb-4">
-                <label htmlFor="progress">Progresso</label>
-                <input
-                    type="text"
-                    id="progress"
-                    name="progress"
-                    value={inputData.progress}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 rounded-md p-1"
-                />
-            </div>
-
-            <div className="grid gap-1 mb-4">
-                <label htmlFor="cnpj">CNPJ</label>
-                <input
-                    type="text"
-                    id="cnpj"
-                    name="cnpj"
-                    value={inputData.cnpj}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 rounded-md p-1"
-                />
-            </div>
-
-
-
-            <button className="w-full rounded-md bg-purple-600 px-4 py-2 font-medium text-lg text-gray-50 transition-colors hover:bg-purple-600/60"
-                aria-label="Salvar Usuário"
-                onClick={handleSubmit}
-            >
-
-                Salvar
-            </button>
+      <div className="grid gap-1 mb-4">
+        <label>Status</label>
+        <div className="flex items-center gap-4">
+          <label className="flex items-center ">
+            <input
+              type="radio"
+              name="status"
+              value="em-desenvolvimento"
+              checked={inputData.status === "em-desenvolvimento"}
+              onChange={handleStatusChange}
+              className="form-radio text-purple-600 cursor-pointer"
+            />
+            <span className="ml-2">Em desenvolvimento</span>
+          </label>
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="status"
+              value="em-testes"
+              checked={inputData.status === "em-testes"}
+              onChange={handleStatusChange}
+              className="form-radio text-purple-600 cursor-pointer"
+            />
+            <span className="ml-2">Em testes</span>
+          </label>
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="status"
+              value="finalizado"
+              checked={inputData.status === "finalizado"}
+              onChange={handleStatusChange}
+              className="form-radio text-purple-600 cursor-pointer"
+            />
+            <span className="ml-2">Finalizado</span>
+          </label>
         </div>
-    );
+      </div>
+
+      <div className="grid gap-1 mb-4">
+        <label htmlFor="progress">Progresso</label>
+        <input
+          type="text"
+          id="progress"
+          name="progress"
+          value={inputData.progress}
+          onChange={handleInputChange}
+          className="border border-gray-300 rounded-md p-1"
+        />
+      </div>
+
+      <div className="grid gap-1 mb-4">
+        <label htmlFor="cnpj">CNPJ</label>
+        <input
+          type="text"
+          id="cnpj"
+          name="cnpj"
+          value={inputData.cnpj}
+          onChange={handleInputChange}
+          className="border border-gray-300 rounded-md p-1"
+        />
+      </div>
+
+      <button
+        className="w-full rounded-md bg-purple-600 px-4 py-2 font-medium text-lg text-gray-50 transition-colors hover:bg-purple-600/60"
+        aria-label="Salvar Usuário"
+        onClick={handleSubmit}
+      >
+        Salvar
+      </button>
+    </div>
+  );
 }
